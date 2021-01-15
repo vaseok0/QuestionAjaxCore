364daf8093710a02a9dae3503727f795faf9debd
vaseok0 <60443985+vaseok0@users.noreply.github.com>
2021-01-15 13:58:59 +0200

    Добавьте файлы проекта.

---
+++ QuestionAjaxCore/Models/Data/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionAjaxCore.Models.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ QuestionAjaxCore/Models/Data/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionAjaxCore.Models.Data
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string Wrong1 { get; set; }
+        public string Wrong2 { get; set; }
+        public string Wrong3 { get; set; }
+    }
+}
